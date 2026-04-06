8b2cbdfdccd594a0b39083b400798a6a858bd2b2
Your Name <inda.richard@gmail.com>
2026-04-05 21:55:27 -0400

    Set up basic prompting to open Ai API

--- chat-ai-api/src/server.ts
+++ chat-ai-api/src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { StreamChat } from "stream-chat";
+import OpenAI from "openai";
 
 dotenv.config();
 
@@ -15,6 +16,11 @@ const PORT = process.env.PORT || 5000;
 
 //Initialize Stream Chat
 const chatClient = StreamChat.getInstance(process.env.STREAM_API_KEY!, process.env.STREAM_API_SECRET!);
+
+//Initialize OpenAI
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+})
 
 //Register user with Stream Chat
 app.post('/register-user',
@@ -39,7 +45,7 @@ app.post('/register-user',
                 });
             }
 
-            res.status(200).json({ userId,name,email });
+            res.status(200).json({ userId, name, email });
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: 'Internal server error' });
@@ -47,6 +53,46 @@ app.post('/register-user',
 
     }
 );
+
+//Send message to Ai
+app.post('/chat', async (req: Request, res: Response): Promise<any> => {
+    const { message, userId } = req.body;
+
+    if (!message || !userId) {
+        return res.status(400).json({ error: 'Message and user are required' });
+    }
+
+    try {
+        const userResponse = await chatClient.queryUsers({ id: userId });
+
+        if (!userResponse.users.length) {
+            return res.status(404).json({ error: 'User not found. Please register first' });
+        }
+
+        //Prompt OpenAi GPT-4o mini
+        const response = await openai.chat.completions.create({ 
+            model: 'gpt-4o-mini', 
+            messages: [{ role: 'user', content: message }] });
+
+            const aiMessage = response.choices[0].message?.content??'No Response from Ai';
+
+            //Create or get channel
+            const channel = chatClient.channel('messaging',`chat-${userId}`, {
+                name: `AI Chat`,
+                created_by_id: 'ai_bot',
+            });
+
+            await channel. create();
+            await channel.sendMessage({text: aiMessage, user_id: 'ai_bot'});
+
+            res.status(200).json({ reply: aiMessage });
+
+    } catch (error) {
+        console.error('Error Generating Ai Response:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+
+});
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
